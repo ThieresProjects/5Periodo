ab523c10ff30c102a2b9458fa3876ffc29ee3abf
ThieresKB <thieresmda@gmail.com>
2024-05-02 19:29:44 -0300

    committft

--- H1/AmigoChocolate/Atividades/Views/components/Home.tsx
+++ H1/AmigoChocolate/Atividades/Views/components/Home.tsx
@@ -11,14 +11,14 @@ import {
 import styles from './styles'
 
 
-const Login = () => {
+const Home = () => {
     const [login, setLogin] = useState('');
     const [senha, setSenha] = useState('');
 
     const navigation = useNavigation<StackTypes>();
 
     const handleLogin = () => {
-        navigation.navigate('Home');
+        navigation.navigate('Login');
     }
 
     const handleTroca = () => {
@@ -35,34 +35,11 @@ const Login = () => {
     <View style={styles.container}>
       <ImageBackground source={image} resizeMode="cover" style={styles.image} >
       {/* <Text style={styles.title}>Amigo Chocolate</Text> */}
-      <TextInput
-        style={styles.input}
-        placeholder="Usuário"
-        placeholderTextColor="#ccc"
-        value={login}
-        onChangeText={(text) => setLogin(text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Senha"
-        placeholderTextColor="#ccc"
-        secureTextEntry={true}
-        value={senha}
-        onChangeText={(text) => setSenha(text)}
-      />
-      <TouchableOpacity style={styles.button} onPress={handleLogin}>
-        <Text style={styles.buttonText}>Login</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={handleTroca}>
-        <Text style={styles.buttonText}>Esqueceu sua senha?</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={handleCadastro}>
-        <Text style={styles.buttonText}>Cadastre-se</Text>
-      </TouchableOpacity>
+
 
       </ImageBackground >
     </View>
   );
 };
 
-export default Login;
+export default Home;
--- H1/AmigoChocolate/Atividades/Views/routes/routes.tsx
+++ H1/AmigoChocolate/Atividades/Views/routes/routes.tsx
@@ -4,13 +4,15 @@ import { NavigationContainer } from "@react-navigation/native";
 import Login from "../components/login";
 import Cadastro from "../components/cadastro";
 import TrocaSenha from "../components/trocaSenha";
+import Home from "../components/Home";
 
 const Stack = createNativeStackNavigator();
 
 type StackNavigation = {
     Cadastro : undefined,
     TrocaSenha : undefined,
-    Login : undefined
+    Login : undefined,
+    Home : undefined
 } 
 
 export type StackTypes = NativeStackNavigationProp<StackNavigation>;
@@ -22,6 +24,7 @@ export default function StackComponent(){
                 <Stack.Screen name="Login" component={Login}/>
                 <Stack.Screen name="TrocaSenha" component={TrocaSenha}/>
                 <Stack.Screen name="Cadastro" component={Cadastro}/>
+                <Stack.Screen name="Home" component={Home}/>
             </Stack.Navigator>
         </NavigationContainer>
     )
